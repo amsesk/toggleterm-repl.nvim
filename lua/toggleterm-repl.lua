367efd7546c1e84c20d18775da1bd487b48f9187
amsesk <ubuntu@ip-10-128-254-37.ec2.internal>
2024-04-10 00:55:41 +0000

    many edits

--- lua/toggleterm-repl.lua
+++ lua/toggleterm-repl.lua
@@ -1,11 +1,23 @@
+local api = vim.api
+local tt = require("toggleterm")
+local nn = require("notebook-navigator")
+local nn_utils = require("notebook-navigator.utils")
+
+local M = {}
 
 local term = require("toggleterm.terminal")
 local Terminal = term.Terminal
-local rlangrepl = { cmd = "R", direction = "horizontal", hidden = false, repl_type = "rlang"} 
-function pyreplopts()
-    return { cmd = "ipython", direction = "horizontal", hidden = false , repl_type = "python"}
-end
-TT_FOCUSED_REPL = nil
+--local rlangrepl = { cmd = "R", direction = "horizontal", hidden = false, repl_type = "rlang"}
+
+function pyreplopts() return { cmd = "ipython --no-autoindent", direction = "horizontal", hidden = false , repl_type = "python"} end
+function rlangreplopts() return { cmd = "R", direction = "horizontal", hidden = false , repl_type = "R"} end
+
+M.REPLOPT = {
+    python = pyreplopts,
+    r = rlangreplopts,
+}
+
+M.active_repl = nil
 
 BFT = vim.bo.filetype
 
@@ -34,40 +46,102 @@ function get_term_by(key, value)
     return matches
 end
 
-function send_line()
-    require("toggleterm").send_lines_to_terminal("single_line", false, {args = TT_FOCUSED_REPL})
+function M.send_line()
+    if M.active_repl == nil then
+        M.new_ft_repl()
+    end
+    require("toggleterm").send_lines_to_terminal("single_line", false, {args = M.active_repl})
 end
 
-function _new_repl(termopts, display_name, make_focused)
+function M.send_lines()
+    if M.active_repl == nil then
+        local current_window = api.nvim_get_current_win()
+        M.new_ft_repl()
+        api.nvim_set_current_win(current_window)
+
+    end
+    require("toggleterm").send_lines_to_terminal("visual_lines", false, {args = M.active_repl}, true)
+end
+
+function M.run_cell_and_move()
+    if M.active_repl == nil then
+        local current_window = api.nvim_get_current_win()
+        M.new_ft_repl()
+        api.nvim_set_current_win(current_window)
+
+    end
+    local repl_args = {
+        id = M.active_repl,
+        -- Trimming spaces here screws up text sent to ipython
+        trim_spaces = false
+        }
+    nn.run_and_move(repl_args)
+end
+
+function M.set_active(id)
+    M.active_repl = id
+end
+
+function M._new_repl(termopts, display_name, make_focused)
     make_focused = make_focused or true
     display_name = display_name or "python"
     termopts["display_name"] = display_name
     termopts["id"] = term_next_id()
     local repl = Terminal:new(termopts)
     if (make_focused) then
-        TT_FOCUSED_REPL = repl["id"]
+        M.active_repl = repl["id"]
     end
     return repl
 end
 
-function _create_or_toggle_repl()
+function M.new_ft_repl()
+    local existing_repls = get_term_by("repl_type", BFT)
+    local repl_display_name = string.format("%s-%s", BFT, #existing_repls+1)
+    M._new_repl(M.REPLOPT[BFT](), repl_display_name, true):open()
+    --if (BFT == "python") then
+    --    M._new_repl(pyreplopts(), repl_display_name, true):open()
+    --end
+end
+
+function M._create_or_toggle_repl()
     _update_buf_ft(vim.bo.filetype)
     if (BFT == "python") then
         local pyrepls = get_term_by("repl_type", BFT)
         if(#pyrepls == 0) then
-            _new_repl(pyreplopts(), "python", true):open()
+            M._new_repl(pyreplopts(), "ipython", true):open()
         else
             if(#pyrepls ==  1) then
                 pyrepls[1]:toggle()
-                TT_FOCUSED_REPL = pyrepls[1]["id"]
+                M.active_repl = pyrepls[1]["id"]
             else
                 vim.cmd("Telescope toggleterm_mananger")
             end
         end
-    elseif(BFT == "R") then
-        rterm:toggle()
+    elseif(BFT == "r") then
+        local rlangrepls = get_term_by("repl_type", BFT)
+        if(#rlangrepls == 0) then
+            M._new_repl(rlangreplopts(), "r", true):open()
+        else
+            if(#rlangrepls ==  1) then
+                rlangrepls[1]:toggle()
+                M.active_repl = rlangrepls[1]["id"]
+            else
+                vim.cmd("Telescope toggleterm_mananger")
+            end
+        end
     else
-        vim.cmd("ToggleTerm")
+        tt.toggle_command()
     end
 end
 
+local function setup_commands()
+    local command = api.nvim_create_user_command
+    command("ToggleTermReplSetActive", function(args) M.set_active(args) end, {nargs = 1})
+    command("ToggleTermFtReplNew", function(args) M.new_ft_repl(args) end, {nargs = 0})
+end
+
+function M.setup(opts)
+    setup_commands()
+end
+
+return M
